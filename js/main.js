d9af7d9e2c22f4a13f4f290775d51e4e6dc14d2a
izanaginookami10 <31240758+izanaginookami10@users.noreply.github.com>
2021-07-01 13:59:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -80,18 +80,18 @@
         }
 
         navigator.mediaDevices.getUserMedia(videoSettings)
+        
             .then(function (stream) {
                 //Setup the video stream
-                video.srcObject = stream;
-            
-            const constraints = {
-             "video": {
-               "facingMode": 
-                  { "ideal": "environment" }
-              }
-            };
+                //video.srcObject = stream
+                const constraints = {
+                 "video": {
+                   "facingMode": 
+                      { "ideal": "environment" }
+                  }
+                };
 
-            const stream = await navigator.mediaDevices.getUserMedia(constraints);
+                const stream = await navigator.mediaDevices.getUserMedia(constraints);
                 video.addEventListener("loadedmetadata", function (e) {
                     //get video width and height as it might be different than we requested
                     pictureWidth = this.videoWidth;
